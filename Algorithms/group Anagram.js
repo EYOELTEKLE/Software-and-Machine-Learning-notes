e1e99aee0820d6ae66727f0970cab232fd05d845
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2022-08-21 14:51:57 +0300

    Update group Anagram.js

--- Algorithms/group Anagram.js
+++ Algorithms/group Anagram.js
@@ -4,24 +4,8 @@ var groupAnagrams = function(strs) {
     for (let i = 0; i < arr.length;i++)
     {
         if (!(arr[i] in obj)) obj[arr[i]] = []
-        obj[arr[i]].push(i)
+        obj[arr[i]].push(strs[i])
     };
-    
-    let val = Object.values(obj);
-    let ans = [];
-    let stack = []
-    for (let i = 0; i < val.length;i++)
-    {
-        for (let j = 0; j < val[i].length;j++)
-        {
-            
-            stack.push(strs[val[i][j]])
-            
-        }
-        ans.push(stack);
-        stack = []
-        
-    }
-   return ans
+   return Object.values(obj)
     
 };
