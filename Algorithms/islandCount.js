5c03e7ad33d6fc2418798fef2fea20e70a1f45b4
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2022-08-25 16:01:52 +0300

    Create islandCount.js

---
+++ Algorithms/islandCount.js
@@ -0,0 +1,43 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+     const visited = new Set();
+    let count = 0;
+    for (let r = 0; r < grid.length; r++)
+    {
+        for (let c = 0; c < grid[r].length;c++)
+        {
+            if (grid[r][c] == "1")
+            {
+               let ans =  explore(grid,r,c,visited);
+                if (ans)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+};
+
+const explore = (grid,r,c,visited) => 
+{
+    const rowBounds = 0 <= r && r < grid.length;
+ 
+    const colBounds = 0 <= c && c < grid[0].length  ;
+    const position = r+','+c;
+
+    if (!rowBounds || !colBounds) return false;
+
+    if (grid[r][c] == "0") return false;
+    if (visited.has(position)) return false;
+    visited.add(position);
+    explore(grid,r - 1,c,visited);
+    explore(grid,r + 1,c,visited);
+    explore(grid,r,c - 1,visited);
+    explore(grid,r,c + 1,visited);
+ 
+    return true;
+}
