706758bb4918f6622e10c4543356d2d897f979f1
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2022-08-27 20:43:52 +0300

    Code Refraction and sort Mutation fix using array destructuring

--- Algorithms/duplicateRemover.js
+++ Algorithms/duplicateRemover.js
@@ -1,26 +1,24 @@
+const nestedDuplicateRemover = (x) => 
+{
+
 let obj =
 {
 
 }
-
-let x = [[1,2,1],[2,1,1],[4,5,12],[12,5,4],[5,5,2]]
 for (let i = 0; i < x.length; i++)
 {
-        let temp = x[i].sort((a,b) => a - b).join('')
+   
+        let temp = [ ...x[i]].sort((a,b) => a - b).join('');
+        
         if (!(temp in obj))
-        {       obj[temp] = i
-                continue;
+        {       obj[temp] = i;
+             
         }
 }
 
 let fixed = Object.values(obj)
-let news =  []
 
-for (let i = 0; i < fixed.length; i++)
-{
-        if (i == fixed[i])
-        {
-                news.push(x[i])
-        }
+let news = fixed.map( y => x[y])
+
+return news;
 }
-console.log(news)
