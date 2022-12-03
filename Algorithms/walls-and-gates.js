97cd8c8b791c72965921966379913183ff9063f8
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2022-12-03 07:41:09 +0300

    Update walls-and-gates.js

--- Algorithms/walls-and-gates.js
+++ Algorithms/walls-and-gates.js
@@ -1,1 +1,32 @@
 
+ function wallsAndGates(rooms) {
+    // write your code here
+
+    let row = rooms.length;
+    let col = rooms[0].length;
+   
+    for (let r = 0; r < row; r++)
+    {
+      for (let c = 0; c < col; c++)
+      {
+        if (rooms[r][c] == 0)
+        {
+          dfs(r,c,0,rooms)
+        }
+      }
+    }
+  }
+
+const dfs = (r,c,count,rooms) => 
+{
+    let bound1 = 0 <= r &&  r < rooms.length;
+    let bound2 = 0 <= c && c < rooms[0].length;
+    if(!bound1 || !bound2) return;
+    if(rooms[r][c] == -1) return;
+    if(count > rooms[r][c]) return;
+    rooms[r][c] = count;
+    dfs(r+1,c,count+1,rooms)
+    dfs(r,c+1,count+1,rooms)
+    dfs(r-1,c,count+1,rooms)
+    dfs(r,c-1,count+1,rooms)
+}
