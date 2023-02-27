8cf59781b5b034e2452c871745d13bcd368b8918
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2023-02-27 16:37:16 +0300

    Update coinChange.js

--- Algorithms/coinChange.js
+++ Algorithms/coinChange.js
@@ -44,3 +44,35 @@ const DP = (coins,amount,mem={}) =>
     mem[amount] = min
     return mem[amount]
 }
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+ /**
+    TOP dOWN recursive solution added
+  **/
+var coinChange = function(coins, amount) {
+    let ans = count(coins,amount);
+
+    return ans == -Infinity ? -1 : ans;
+};
+
+
+const count = (coins,amount,mem={}) => 
+{
+   if (amount in mem) return mem[amount];
+   if (amount == 0) return 0;
+    if (amount < 0) return null;
+    let min = -Infinity;
+
+    for (let i = 0; i < coins.length; i++)
+    {
+        let ans = count(coins,amount - coins[i],mem);
+        if((min == -Infinity || min > ans) && ans != null && ans != -Infinity)min = 1 + ans;       
+        
+    }
+    mem[amount] = min;
+    return mem[amount];
+}
