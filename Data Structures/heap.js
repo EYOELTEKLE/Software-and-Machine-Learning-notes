bd20c90680748da9a3997a8915c77a0a3ff46717
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2023-05-09 16:35:43 +0300

    Update heap.js

--- Data Structures/heap.js
+++ Data Structures/heap.js
@@ -1,63 +1,55 @@
 class maxHeap
 {
-    constructor(array)
+    constructor()
     {
-        this.heap=[];
-        this.heapify(array);
+        this.heap = [];
+    }
+    buildHeap(array)
+    {
+        let length = array.length;
+        let parent = Math.floor(length/2) - 1;
+        for (let i = parent; i >= 0; i--)
+        {
+            this.bubbleDown(array,i);
+        }
+        this.heap = array;
+        return this;
+    }
+    bubbleDown(array,index)
+    {
+        const length = array.length;
+        let current = array[index];
+      
+
         
-    }
-    heapify(l)
-    {
-    let length = l.length;
-    let last = Math.floor(length/2) -1;
-    for (let i = last; i >= 0;i--)
-    {
-        this.bubbleDown(l,i);
-    }
-    this.heap = l;
-    return this;
-        
-    }
-    bubbleDown(l,i)
-    {
-        let length = l.length;
-        let current = l[i];
         while(true)
         {
-         let leftIdx = 2*i + 1;
-         let rightIdx = 2*i + 2;
-         let leftChild,rightChild;
-         let largest = null;
-         if(leftIdx < length)
-         {
-             leftChild = l[leftIdx];
-             if(leftChild > current)
-             {
-                 largest = leftIdx;
-             }
-         }
-        if(rightIdx < length)
-        {
-            rightChild = l[rightIdx];
-            if((largest == null && rightChild > current) || (largest != null && rightChild > leftChild))
-               {
-                largest = rightIdx;
-               }
+    	let leftChildIdx = 2 * index + 1;
+        let rightChildIdx = 2 * index + 2;
+        let largest = null;
+        let leftChild,rightChild;
+            if(leftChildIdx < length)
+            {
+                leftChild = array[leftChildIdx];
+                if(leftChild > current)
+                {
+                    largest = leftChildIdx;
+                }  
+            }
+            if(rightChildIdx < length)
+            {
+                rightChild = array[rightChildIdx];
+                if((rightChild > current) && (largest == null) || (largest != null) && (rightChild > leftChild))
+                {
+                    largest = rightChildIdx;
+                }
+            }
+            if(largest === null)break;
+            array[index] = array[largest];
+            array[largest] = current;
+            index = largest;
         }
-
-        if(!largest) break;
-        l[i] = l[largest];
-        l[largest] = current;
-        i = largest;
-            
-            
-        }
-    }
-    insert(val)
-    {
-        this.heap.push(val);
-        this.bubbleUp();
-        return this;
+        
     }
     peek()
     {
@@ -65,27 +57,35 @@ class maxHeap
     }
     extractMax()
     {
-        let max = this.heap[0];
-        let last = this.heap.pop();
+        const maximumValue = this.heap[0];
+        const last = this.heap.pop();
         if(this.heap.length > 0)
         {
             this.heap[0] = last;
             this.bubbleDown(this.heap,0);
         }
-        return max;
+        return maximumValue;
+    }
+    insert(value)
+    {
+        this.heap.push(value);
+        this.bubbleUp();
+        return this;
     }
     bubbleUp()
     {
         let idx = this.heap.length - 1;
-        const val = this.heap[idx];
-        while (idx > 0)
+        const value = this.heap[idx];
+        while(idx > 0)
         {
-            let parentIdx = Math.floor((idx-1)/2);
-            let parentVal = this.heap[parentIdx];
-            if(val <= parentVal) break;
+            let parent = Math.floor((idx-1)/2);
+            let parentVal = this.heap[parent];
+            if(value <= parentVal)break;
+            this.heap[parent] = value;
             this.heap[idx] = parentVal;
-            this.heap[parentIdx] = val;
-            idx = parentIdx;
+            idx = parent;
         }
     }
+    
+    
 }
