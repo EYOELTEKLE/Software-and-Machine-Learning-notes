8c8fbedbd926cf8aacaeb134bdde160b4877ca8b
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2023-05-14 19:42:55 +0300

    Update priorityQueue.js

--- Data Structures/priorityQueue.js
+++ Data Structures/priorityQueue.js
@@ -1,91 +1,91 @@
-/**
-Min priority Queue
-Time Complexity - O(logn) - (enqueue,dequeue)
-Space Complexity - O(1) - (dequeue,enqueue)
-**/
-class Node
-{
-    constructor(value,priority)
-    {
+class Node {
+    constructor(value, priority) {
         this.value = value;
         this.priority = priority;
     }
-    
 }
-class PriorityQueue
-{
-    constructor()
-    {
-        this.heap = [];
+class priorityQueue {
+    constructor() {
+        this.priorityQueue = []
     }
-    enqueue(val,priority)
-    {
-        let node = new Node(val,priority);
-        this.heap.push(node);
-        this.bubbleUp();
-    }
-    bubbleUp()
-    {
-        let idx = this.heap.length - 1;
-        const current = this.heap[idx];
-        while(idx > 0)
-        {
-            let parentIdx = Math.floor((idx - 1)/2);
-            let parent = this.heap[parentIdx];
-
-            if(current.priority >= parent.priority) break;
-            this.heap[idx] = parent;
-            this.heap[parentIdx] = current;
-            idx = parentIdx;
-          
-            
+    build(array) {
+        let length = array.length;
+        let lastParent = Math.floor(length / 2) - 1;
+        for (let i = lastParent; i >= 0; i--) {
+            this.bubbleDown(array, i);
         }
+        this.priorityQueue = array;
+        return this;
     }
-       dequeue()
-    {
-           const min = this.heap[0];
-           const last = this.heap.pop();
-        if(this.heap.length > 0)
-        {
-            this.heap[0] = last;
-            this.bubbleDown();
+    enqeue(val, priority) {
+        let node = new Node(val, priority);
+        this.priorityQueue.push(node);
+        this.bubbleUp();
+        return this;
+    }
+    dequeue() {
+        const min = this.priorityQueue[0];
+        const last = this.priorityQueue.pop();
+        if (this.priorityQueue.length > 0) {
+            this.priorityQueue[0] = last;
+            this.bubbleDown(this.priorityQueue,0);
         }
         return min;
-        
     }
-    bubbleDown()
-    {
-        let idx = 0;
-        let length = this.heap.length;
-        let current = this.heap[0];
-        while(true)
-        {
-                let leftIdx = 2*idx + 1;
-                let rightIdx = 2*idx + 2;
-                let rightChild,leftChild;
-                let smallest = null;
+    bubbleDown(arr, idx) {
+        if (!arr) {
+            let arr = this.priorityQueue;
+        };
+        let val = arr[idx];
 
-                if(leftIdx < length)
-                {
-                    leftChild = this.heap[leftIdx];
-                    if(leftChild.priority < current.priority)
-                    {
-                        smallest = leftIdx;
-                    }
-                }    
-                if(rightIdx < length)
-                {
-                        rightChild = this.heap[rightIdx];
-                if((smallest == null && rightChild.priority < current.priority) || (smallest != null && rightChild.priority < leftChild.priority))
-                        {
-                           smallest =  rightIdx;
-                        } 
+
+        while (true) {
+            let leftChild = 2 * idx + 1;
+            let rightChild = 2 * idx + 2;
+            let smallest = null;
+            let left, right;
+
+            if (leftChild < arr.length) {
+                left = arr[leftChild];
+
+                if (left.priority < val.priority) {
+                    smallest = leftChild;
+
                 }
-            if(smallest === null) break;
-            this.heap[idx] = this.heap[smallest];
-            this.heap[smallest] = current;
+            }
+            if (rightChild < arr.length) {
+                right = arr[rightChild];
+                if ((smallest == null && right.priority < val.priority) || (smallest != null && right.priority < left.priority)) {
+                    smallest = rightChild;
+                }
+            }
+            if (smallest == null)break;
+            [arr[idx], arr[smallest]] = [arr[smallest], arr[idx]];
             idx = smallest;
+
         }
-        
+    }
+    bubbleUp() {
+        let idx = this.priorityQueue.length - 1;
+        let element = this.priorityQueue[idx];
+
+        while (idx > 0) {
+
+            let parent = Math.floor((idx - 1) / 2);
+            let parentVal = this.priorityQueue[parent];
+            if (parentVal.priority <= element.priority) break;
+
+            [this.priorityQueue[idx], this.priorityQueue[parent]] = [this.priorityQueue[parent], this.priorityQueue[idx]];
+            idx = parent;
+        }
+    }
+    peek() {
+        return this.priorityQueue[0];
     }
 }
+let queue = new priorityQueue();
+/**usage
+queue.enqueue([10,120],100) //arguments -> (value,priority)
+or
+queue.build([{value:[10,100],priority:2},{value:[100,100],priority:5}])
+**/
