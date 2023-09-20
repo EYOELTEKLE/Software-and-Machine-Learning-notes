144866c83a8f3cbe85a6d469a54950615332dfa7
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2023-09-20 16:09:53 +0300

    Create segmentTree.ts

---
+++ Data Structures/segmentTree.ts
@@ -0,0 +1,96 @@
+
+
+class node
+{
+    sum:number;
+    left:null | node;
+    right:null | node;
+    l:number;
+    r:number;
+    constructor(left:number,right:number)
+    {
+        this.sum = 0;
+        this.left = null;
+        this.right = null;
+        this.l = left;
+        this.r = right;
+    }
+}
+
+
+class SegmentTree {
+
+    root:node | null;
+   constructor()
+    {
+        this.root=null;
+    }
+    build(arr:number[])
+    {
+        let left = 0;
+        let right = arr.length-1;
+        let temp = this.create(arr,left,right);
+        this.root = temp;
+        return this.root;
+    }
+    create(arr:number[],left:number,right:number)
+    {
+        if(left == right)
+        {
+            let temp = new node(left,right);
+            temp.sum  = arr[left];
+            return temp;
+        }
+        let temp = new node(left,right);
+        let m = Math.floor((left+right)/2);
+        temp.left = this.create(arr,left,m);
+        temp.right = this.create(arr,m+1,right);
+        temp.sum = temp.left.sum + temp.right.sum;
+        return temp;
+    }
+    update(index:number,val:number,root=this.root)
+    {
+       
+        if(root?.l ==index &&  root?.r == index)
+        {
+                root.sum = val;
+                return;
+        }
+        let m = Math.floor((root!.l + root!.r)/2);
+
+        if (index > m)
+        {
+                this.update(index,val,root!.right);
+        }
+        else
+        {
+                this.update(index,val,root!.left);
+        }
+        root!.sum =root!.left!.sum + root!.right!.sum;
+       
+    }
+    rangeQuery(l:number,r:number,root=this.root):number
+    {
+        if(root!.l == l && root!.r == r)
+        {
+                return root!.sum;
+        }
+        let m = Math.floor((root!.l + root!.r)/2);
+        if(l > m)
+        {
+                return this.rangeQuery(l,r,root!.right);
+        }
+        else if(r <= m)
+        {
+                return this.rangeQuery(l,r,root!.left);
+        }
+        else
+        {
+                return this.rangeQuery(l,m,root!.left) + this.rangeQuery(m+1,r,root!.right)
+        }
+    }
+}  
+
+const seg = new SegmentTree();
+seg.build([1,3,5,7,9,11]);
+seg.update(3,33);
